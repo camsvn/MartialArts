ea3fc2ced079a3721d24557f4b48448222788560
Amal Salvin <amalsalvin97@gmail.com>
2020-09-22 12:26:26 +0530

    Attendance Sheet Color Log Added

--- src/components/AttendanceCard.js
+++ src/components/AttendanceCard.js
@@ -1,18 +1,117 @@
 import React, { Component } from "react"
-import { Card, Calendar, Select, Radio, Col, Row } from "antd"
+import { Card, Calendar, Select, Radio, Col, Row, Popconfirm } from "antd"
 import "./AttendanceCard.scss"
+import { iteratee } from "lodash"
 
 const { Meta } = Card
 const { Group, Button } = Radio
 
+let attendancePresent = [
+    "01/09/2020",
+    "05/09/2020",
+    "08/09/2020",
+    "10/09/2020",
+    "15/09/2020",
+    "19/09/2020",
+    "04/08/2020",
+    "18/08/2020",
+]
+
+let attendanceAbsent = [
+    "03/09/2020",
+    "12/09/2020",
+    "17/09/2020",
+    "20/08/2020",
+    "15/08/2020",
+]
+
+function addAttendancePresent(value){
+    let date =value.toDate().toLocaleDateString("en-GB")
+    attendanceAbsent.includes(date) && attendanceAbsent.splice(attendanceAbsent.findIndex(e => e === date),1)
+    attendancePresent.push(date)
+    // console.log(attendancePresent, attendanceAbsent)
+}
+
+function addAttendanceAbsent(value){
+    let date =value.toDate().toLocaleDateString("en-GB")
+    attendancePresent.includes(date) && attendancePresent.splice(attendancePresent.findIndex(e => e === date),1)
+    attendanceAbsent.push(date)
+    // console.log(attendancePresent, attendanceAbsent)
+}
+
+function resetAttendance(value, type){
+    let date = value.toDate().toLocaleDateString("en-GB")
+    type === "P" && attendancePresent.splice(attendancePresent.findIndex(e => e === date),1)
+    type === "A" && attendanceAbsent.splice(attendanceAbsent.findIndex(e => e === date),1)
+    // console.log(attendancePresent, attendanceAbsent)
+}
 export default class AttendanceCard extends Component{
     constructor(props) {
         super(props)
         this.state = {}
-    }
-
+    }     
+    
     onPanelChange(value, mode) {
         console.log(value, mode)
+    }
+
+    // onSelect(value){                    
+    // let date = value.toDate().toLocaleDateString("en-GB")
+    // !attendancePresent.includes(date) && attendancePresent.push(date)
+    // console.log(attendancePresent)
+    // }
+
+    dateCellRender(value){
+        let date = ("0"+value.date()).slice(-2)
+        let Present = attendancePresent.includes(value.toDate().toLocaleDateString("en-GB"))
+        let Absent = attendanceAbsent.includes(value.toDate().toLocaleDateString("en-GB"))
+        if(Present)
+            return(
+                <Popconfirm
+                    title="Register Attendance"
+                    okText="Reset"
+                    cancelText="Absent"
+                    onConfirm={()=>resetAttendance(value,"P")}
+                    onCancel={()=>addAttendanceAbsent(value)}
+                >
+                    <div className="ant-fullcalendar-date">
+                        <div className="ant-fullcalendar-value" style={{background: "#b7eb8f"}} >{date}</div>
+                        <div className="ant-fullcalendar-content"/>
+                    </div>
+                </Popconfirm>
+            )
+        
+        if(Absent)
+            return(
+                <Popconfirm
+                    title="Register Attendance"
+                    okText="Reset"
+                    cancelText="Present"
+                    onConfirm={()=>resetAttendance(value,"A")}
+                    onCancel={()=>addAttendancePresent(value)}
+                >
+                    <div className="ant-fullcalendar-date">
+                        <div className="ant-fullcalendar-value" style={{background:"#ffa39e"}}>{date}</div>
+                        <div className="ant-fullcalendar-content"/>
+                    </div>
+                </Popconfirm>
+            )
+
+        if(!Present||!Absent)
+            return(
+                <Popconfirm
+                    title="Register Attendance"
+                    okText="Present"
+                    cancelText="Absent"
+                    onConfirm={(e)=>addAttendancePresent(value)}
+                    onCancel={()=>addAttendanceAbsent(value)}
+                >
+                    <div className="ant-fullcalendar-date">
+                        <div className="ant-fullcalendar-value" >{date}</div>
+                        <div className="ant-fullcalendar-content"/>
+                    </div>
+                </Popconfirm>
+            )
     }
 
     renderCalendar = () =>{
@@ -24,7 +123,6 @@ export default class AttendanceCard extends Component{
                         const start = 0
                         const end = 12
                         const monthOptions = []
-    
                         const current = value.clone()
                         const localeData = value.localeData()
                         const months = []
@@ -95,6 +193,8 @@ export default class AttendanceCard extends Component{
                         )
                     }}
                     onPanelChange={this.onPanelChange}
+                    onSelect={this.onSelect}
+                    dateFullCellRender={this.dateCellRender}
                 />
             </div>
         )
@@ -109,9 +209,9 @@ export default class AttendanceCard extends Component{
             >
                 <Meta title="Student Name" style={{marginBottom: 5}}/>
                 <ul className="additional" style={{marginBottom:0}}>
-                    <li><span>Total Classes</span>&nbsp;&#58;&nbsp;&nbsp;<span>16</span></li>
-                    <li><span>Classes Attended</span>&nbsp;&#58;&nbsp;&nbsp;<span>9</span></li>
-                    <li><span>Classes Absent</span>&nbsp;&#58;&nbsp;&nbsp;<span>7</span></li>
+                    <li><span>Total Classes</span>&nbsp;&#58;&nbsp;&nbsp;<span>{attendancePresent.length + attendanceAbsent.length}</span></li>
+                    <li><span>Classes Attended</span>&nbsp;&#58;&nbsp;&nbsp;<span>{attendancePresent.length}</span></li>
+                    <li><span>Classes Absent</span>&nbsp;&#58;&nbsp;&nbsp;<span>{attendanceAbsent.length}</span></li>
                 </ul>
             </Card>
             
